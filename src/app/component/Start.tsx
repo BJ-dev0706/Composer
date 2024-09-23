71722c69eb7f971a23d4f2d03447e15d4bde399a
Chi Kit <kitchi734@gmail.com>
2024-09-23 14:18:35 -0700

    omg

---
+++ src/app/component/Start.tsx
@@ -0,0 +1,102 @@
+const Start = () => {
+    return(
+        <div>
+            <div className="start">
+                <div className="text-black intro wf-section">
+                    <div className="inner">
+                        <div className="intro-text">
+                            <h1 className="intro__headline">Meet<br /><span className="giga">Composer</span></h1>
+                            <div className="intro__description">Build trading algorithms with AI, backtest them, then execute—all in one platform. No coding skills required.<br /><br />Trading. Built better.</div>
+                            <div className="md:border border-black rounded-full flex flex-col md:flex-row items-center w-full md:w-fit md:bg-white"><a className="link-arrow-group text-left flex justify-between items-center py-4 px-9 leading-none w-full md:w-fit bg-black text-white rounded-full text-2xl md:text-[36px] link-with-arrow-container" href="https://app.composer.trade/register">Get started<div className="flex items-center justify-center"><span className="link-arrow-left"><svg width="192" height="192" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg" style={{width:"28px", height:"28px"}}><g clipPath="url(#a)"><path d="m8.49 13.263-1.278-1.264 4.439-4.44H.5V5.714h11.15L7.213 1.281 8.49.01l6.627 6.626-6.627 6.627Z" fill="currentColor"></path></g></svg></span><span className="link-arrow-right"><svg width="192" height="192" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg" style={{width:"28px", height:"28px"}}><g clipPath="url(#a)"><path d="m8.49 13.263-1.278-1.264 4.439-4.44H.5V5.714h11.15L7.213 1.281 8.49.01l6.627 6.626-6.627 6.627Z" fill="currentColor"></path></g></svg></span></div></a></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="blocks-parent">
+                    <div className="block-container">
+                        <div className="div-block-151">
+                            <div className="div-block-148 block-col">
+                                <div className="div-block-147">
+                                    <div className="block-little bg-green-light multiply"></div>
+                                    <div className="block-little bg-green-light multiply"></div>
+                                    <div className="block-little bg-green-light multiply"></div>
+                                    <div className="block-little bg-pink hardlight"></div>
+                                    <div className="block-little bg-pink hardlight"></div>
+                                </div>
+                                <div className="div-block-147 line-even">
+                                    <div className="block-little multiply bg-yellow-light"></div>
+                                    <div className="block-little multiply bg-yellow-light"></div>
+                                    <div className="block-little multiply bg-yellow-light"></div>
+                                    <div className="block-little bg-green-light multiply"></div>
+                                    <div className="block-little bg-green-light multiply"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="div-block-149">
+                            <div className="block-row">
+                                <div className="block"></div>
+                                <div className="block bg-pink hardlight"></div>
+                            </div>
+                            <div className="block-row">
+                                <div className="block bg-green-light multiply dotgrid animate animate-4"></div>
+                                <div className="block"></div>
+                            </div>
+                            <div className="block-row">
+                                <div className="block"></div>
+                                <div className="block block--3">
+                                    <div className="bg-pink hardlight h-50 dotgrid animate animate-1"></div>
+                                    <div className="h-50 _w-50 bg-blue hardlight animate-half animate-4"></div>
+                                </div>
+                                <div className="block block--6">
+                                    <div className="h-50 _w-50"></div>
+                                    <div className="_w-25 h-25 hardlight bg-blue"></div>
+                                </div>
+                            </div>
+                            <div className="block-row">
+                                <div className="block bg-green-light multiply animate animate-2"></div>
+                                <div className="block block--4">
+                                    <div className="h-50 d"></div>
+                                    <div className="h-50 _w-50"></div>
+                                    <div className="h-50 _w-50 bg-yellow-light multiply dotgrid animate-half animate-3"></div>
+                                </div>
+                                <div className="block block--5">
+                                    <div className="h-50 _w-50 bg-pink hardlight dotgrid"></div>
+                                    <div className="h-50 _w-50"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 multiply bg-green-light"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 multiply bg-green-light"></div>
+                                    <div className="h-25 _w-25 bg-yellow-light multiply"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 hardlight bg-pink"></div>
+                                </div>
+                            </div>
+                            <div className="block-row">
+                                <div className="block"></div>
+                                <div className="block">
+                                    <div className="bg-pink hardlight h-50 dotgrid animate animate-4"></div>
+                                    <div className="float-right h-50 _w-50 bg-green-light multiply"></div>
+                                </div>
+                                <div className="block block--5">
+                                    <div className="h-50 _w-50 bg-blue hardlight"></div>
+                                    <div className="h-50 _w-50"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 multiply bg-green-light"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 multiply bg-yellow-light"></div>
+                                    <div className="_w-25 h-25"></div>
+                                    <div className="_w-25 h-25 multiply bg-yellow-light"></div>
+                                </div>
+                            </div>
+                            <div className="block-row">
+                                <div className="block bg-blue hardlight"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Start;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Main from "./component/Main";
+// import Main from "./component/Main";
 import Bg_white from "./component/Bg_white";
 import Bg_soft from "./component/Bg_soft";
 import Bg_dark from "./component/Bg-dark";
@@ -8,6 +8,8 @@ import Customize from "./component/Customize";
 import About from "./component/About";
 import Footer from "./component/Footer";
 import Testimonial from "./component/Testimonials";
+import Start from "./component/Start";
+import Header from "./component/Header";
 
 export default function Home() {
   return (
@@ -17,16 +19,19 @@ export default function Home() {
           Get the app →
         </Link>
       </div>
-      <div className="bg-[#ecedee]">
-        <Main />
-        <Bg_white />
-        <Bg_soft />
-        <Bg_dark />
-        <Bg_gray />
-        <Customize />
-        <Testimonial />
-        <About />
-        <Footer />
+      <div className="flex relative flex-col min-h-full bg-grey-soft bg-grid [--grid-opacity:0.16] [--grid-size:240px]">
+        <Header />
+        <main className="min-h-[95vh] overflow-x-clip">
+          <Start />
+          <Bg_white />
+          <Bg_soft />
+          <Bg_dark />
+          <Bg_gray />
+          <Customize />
+          <Testimonial />
+          <About />
+          <Footer />
+        </main>
       </div>
     </div>
   );
